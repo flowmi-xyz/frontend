e7fa5f8ff391d08d66a484f307f09933ccafaa97
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-02 16:50:57 -0300

    feat: starting with NavbarConnectedComponent

---
+++ app/components/NavbarConnected.tsx
@@ -0,0 +1,56 @@
+import { Link } from "@remix-run/react";
+
+import { Flex, Text, Button } from "@chakra-ui/react";
+
+type NavbarConnectedProps = {
+  address: string;
+};
+
+const NavbarConnected = ({ address }: NavbarConnectedProps) => {
+  return (
+    <Flex
+      justify="space-around"
+      bgGradient="linear(to-r, #31108F, #7A3CE3, #E53C79, #E8622C, #F5C144)"
+      height="76px"
+      align="center"
+      alignItems="center"
+      width="100%"
+    >
+      <Link to="/">
+        <Text fontWeight="700" fontSize="20" color="white">
+          Social Defi
+        </Text>
+      </Link>
+
+      <Flex flexDirection="row" alignItems="center" justifyContent="center">
+        <Flex display="flex" align="center">
+          <Button
+            bg="lens"
+            borderRadius="10px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+          >
+            <Text fontWeight="600" fontSize="18" color="lensDark">
+              @cristianvaldivia.lens
+            </Text>
+          </Button>
+
+          <Button
+            bg="white"
+            borderRadius="10px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+            ml="20px"
+          >
+            <Text fontSize="15" color="grayLetter">
+              {`${address.slice(0, 6)} ... ${address.slice(
+                address.length - 4,
+                address.length
+              )}`}
+            </Text>
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NavbarConnected;
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,5 +1,28 @@
+// BFF components
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+// UI components
 import { Box } from "@chakra-ui/react";
 
+import { getSession } from "~/bff/session";
+import NavbarConnected from "~/components/NavbarConnected";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // Get address from cookie session
+  const session = await getSession(request.headers.get("Cookie"));
+
+  const address = session.get("address");
+
+  return address;
+};
+
 export default function Dashboard() {
-  return <Box>Wallet conectada</Box>;
+  const address = useLoaderData();
+
+  return (
+    <Box>
+      <NavbarConnected address={address} />
+    </Box>
+  );
 }
