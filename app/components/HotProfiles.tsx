4d9dc684484721bcfdcbd1a015eb59ac8ec0a2d8
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-03 00:11:44 -0300

    feat: box for one hot profile

--- app/components/HotProfiles.tsx
+++ app/components/HotProfiles.tsx
@@ -1,4 +1,12 @@
-import { Box, Center, Flex, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 
 const HotProfiles = () => {
   return (
@@ -21,16 +29,91 @@ const HotProfiles = () => {
       </Center>
 
       <Center>
-        <Box
+        <Flex
           bg="white"
-          width="500px"
           border="1px"
           borderColor="#E0E0E3"
           boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
           borderRadius="10px"
+          p="5"
         >
-          @fabri.lens
-        </Box>
+          <Box bg="#F2F4F6" width="200px" borderRadius="10px" p="3">
+            <Flex>
+              <Avatar
+                size="md"
+                name="Dan Abrahmov"
+                src="https://bit.ly/dan-abramov"
+              />
+
+              <Box my="auto" pl="2">
+                <Text
+                  fontWeight="700"
+                  fontSize="16px"
+                  lineHeight="120%"
+                  letterSpacing="-0.03em"
+                  color="black"
+                >
+                  @dan_abramov
+                </Text>
+
+                <Text
+                  fontWeight="400"
+                  fontSize="12px"
+                  lineHeight="100%"
+                  letterSpacing="-0.03em"
+                  color="black"
+                  pt="2"
+                >
+                  47k followers
+                </Text>
+              </Box>
+            </Flex>
+
+            <Center pt="5" pb="5">
+              <Text
+                fontWeight="700"
+                fontSize="16px"
+                letterSpacing="-0.03em"
+                color="black"
+              >
+                <Text
+                  as="span"
+                  fontSize="15px"
+                  fontWeight="bold"
+                  bgGradient="linear(to-r, #31108F, #7A3CE3, #E53C79, #E8622C, #F5C144)"
+                  bgClip="text"
+                >
+                  9 MATIC{" "}
+                </Text>
+                accumulated
+              </Text>
+            </Center>
+
+            <Box
+              bg="lens"
+              borderRadius="10px"
+              boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+            >
+              <Flex>
+                <Image
+                  src="./assets/LOGO__lens_ultra small icon.png"
+                  alt="lens"
+                  boxSize="40px"
+                />
+
+                <Text
+                  fontWeight="700"
+                  fontSize="10px"
+                  lineHeight="120%"
+                  color="lensDark"
+                  my="auto"
+                >
+                  Follow @dan_abramov
+                </Text>
+              </Flex>
+            </Box>
+          </Box>
+        </Flex>
       </Center>
     </>
   );
