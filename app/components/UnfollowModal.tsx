e56d88ff8b3d3b5f9b31154faf27e0ff4ebfd04a
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-07 22:33:11 -0300

    feat: start with UnfollowModal

---
+++ app/components/UnfollowModal.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const UnfollowModal = () => {
+  return (
+    <Box width="600px">
+      <Text fontSize={"2xl"} fontWeight={"bold"} mb={4}>
+        Unfollow
+      </Text>
+    </Box>
+  );
+};
+
+export default UnfollowModal;
