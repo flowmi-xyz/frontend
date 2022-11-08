0173d2e20ef9fe11c8411f299d1b4b14c3c55959
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-08 00:03:59 -0300

    feat: deploy with chakra steps

--- app/components/UnfollowModal.tsx
+++ app/components/UnfollowModal.tsx
@@ -2,6 +2,7 @@ import {
   Alert,
   AlertIcon,
   AlertTitle,
+  Box,
   Button,
   Center,
   HStack,
@@ -23,6 +24,7 @@ import { createUnfollowTypedData } from "~/web3/lens/follow/unfollow";
 import { LENS_HUB_ABI } from "~/web3/lens/lens-hub";
 import { getSigner } from "~/web3/etherservice";
 import React from "react";
+import { Step, Steps, useSteps } from "chakra-ui-steps";
 
 type UnFollowModalProps = {
   isOpen: boolean;
@@ -31,6 +33,16 @@ type UnFollowModalProps = {
 };
 
 const UnfollowModal = ({ isOpen, onClose, profileId }: UnFollowModalProps) => {
+  const steps = [
+    { label: "Approve move tokens" },
+    { label: "Deposit tokens in Aave protocol" },
+    { label: "Finish proccess! 🎉" },
+  ];
+
+  const { nextStep, activeStep, reset } = useSteps({
+    initialStep: 0,
+  });
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [signed, setSigned] = React.useState(false);
   const [error, setError] = React.useState(false);
@@ -49,6 +61,8 @@ const UnfollowModal = ({ isOpen, onClose, profileId }: UnFollowModalProps) => {
       LENS_HUB_ABI,
       getSigner()
     );
+
+    nextStep();
 
     try {
       const burnTx = await followNFTContract.burn(typedData.value.tokenId);
@@ -71,6 +85,19 @@ const UnfollowModal = ({ isOpen, onClose, profileId }: UnFollowModalProps) => {
         <ModalHeader>Unfollow</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          <Box>
+            <Steps
+              labelOrientation="vertical"
+              activeStep={activeStep}
+              responsive={false}
+              size="sm"
+            >
+              {steps.map(({ label }) => (
+                <Step label={label} key={label}></Step>
+              ))}
+            </Steps>
+          </Box>
+
           <Text>Are you sure you want to unfollow this user?</Text>
 
           <Button onClick={handleUnfollow}>Unfollow</Button>
@@ -84,27 +111,27 @@ const UnfollowModal = ({ isOpen, onClose, profileId }: UnFollowModalProps) => {
 
           {signed && (
             <Center>
-              <VStack paddingTop={10}>
+              <VStack paddingTop="10">
                 <Alert
-                  marginBottom={5}
+                  marginBottom="5"
                   status="info"
-                  borderRadius={15}
-                  width={"80%"}
+                  borderRadius="15"
+                  width="80%"
                 >
                   <AlertIcon />
-                  <AlertTitle fontWeight={"light"}>
+                  <AlertTitle fontWeight="light">
                     The transaction is being processed.
                   </AlertTitle>
                 </Alert>
 
-                <HStack paddingLeft={10}>
-                  <Text fontWeight={700}>
+                <HStack paddingLeft="10">
+                  <Text fontWeight="700">
                     Waiting transacctions to be mined...
                   </Text>
 
                   <Image
                     src="https://feature.undp.org/beyond-bitcoin/es/assets/mbNja7QNnr/block3.gif"
-                    width={"50%"}
+                    width="50%"
                   />
                 </HStack>
               </VStack>
--- app/style/theme.ts
+++ app/style/theme.ts
@@ -1,5 +1,56 @@
 import { extendTheme } from "@chakra-ui/react";
 
+import { StepsStyleConfig } from "chakra-ui-steps";
+
+const CustomSteps = {
+  ...StepsStyleConfig,
+  baseStyle: (props: any) => {
+    return {
+      ...StepsStyleConfig.baseStyle(props),
+      icon: {
+        ...StepsStyleConfig.baseStyle(props).icon,
+        // your custom styles here
+        strokeWidth: "2px",
+        color: "white",
+      },
+      stepIconContainer: {
+        ...StepsStyleConfig.baseStyle(props).stepContainer,
+        // your custom styles here
+        bg: "#757575",
+        borderColor: "#757575",
+        color: "white",
+        borderRadius: "50%",
+        alignItems: "center",
+        justifyContent: "center",
+        _highlighted: {
+          bg: "#F72585",
+          borderColor: "#F72585",
+        },
+      },
+      connector: {
+        ...StepsStyleConfig.baseStyle(props).connector,
+        // your custom styles here
+        borderColor: "#757575",
+        opacity: 0.3,
+        _highlighted: {
+          borderColor: "#F72585",
+          opacity: 1.0,
+        },
+      },
+      labelContainer: {
+        ...StepsStyleConfig.baseStyle(props).labelContainer,
+        // your custom styles here
+        width: "45%",
+      },
+      label: {
+        ...StepsStyleConfig.baseStyle(props).label,
+        // your custom styles here
+        fontSize: "sm",
+      },
+    };
+  },
+};
+
 export const theme = extendTheme({
   colors: {
     first: "#31108F",
@@ -11,6 +62,9 @@ export const theme = extendTheme({
     lens: "#BDFC5A",
     lensDark: "#00501E",
     twitter: "#1DA1F2",
+  },
+  components: {
+    Steps: CustomSteps,
   },
 });
 
