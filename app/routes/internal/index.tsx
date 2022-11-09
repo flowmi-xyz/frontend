a9bcae7c71330e49a5ab23fb05ee6ca6b0ff92eb
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-09 17:09:20 -0300

    feat: use react to get element from components

--- app/routes/internal/index.tsx
+++ app/routes/internal/index.tsx
@@ -5,6 +5,8 @@ import { useLoaderData } from "@remix-run/react";
 import { getSession } from "~/bff/session";
 
 // UI components
+import React from "react";
+
 import {
   Box,
   Button,
@@ -13,16 +15,13 @@ import {
   Image,
   Input,
   InputGroup,
-  InputLeftAddon,
   InputRightAddon,
   Select,
-  Stack,
   Text,
 } from "@chakra-ui/react";
 
 // components
 import NavbarConnected from "~/components/NavbarConnected";
-import { WIDTH_FEED } from "~/style/theme";
 
 export const loader: LoaderFunction = async ({ request }) => {
   // Get address from cookie session
@@ -40,11 +39,29 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Index() {
   const { address } = useLoaderData();
 
+  const [handle, setHandle] = React.useState("");
+  const [profilePicture, setProfilePicture] = React.useState(null);
+  const [followNFT, setFollowNFT] = React.useState(null);
+  const [followModule, setFollowModule] = React.useState(null);
+
+  const handleChangehandle = (event: any) => {
+    setHandle(event.target.value);
+  };
+
+  const handleCreateProfile = async () => {
+    console.log("Create profile");
+
+    console.log(handle);
+    console.log(profilePicture);
+    console.log(followNFT);
+    console.log(followModule);
+  };
+
   return (
     <Box>
       <NavbarConnected address={address} authenticatedInLens={false} />
 
-      <Box maxWidth="600px" m="auto" pt="3" pb="3" width={WIDTH_FEED}>
+      <Box maxWidth="600px" m="auto" pt="3" pb="3">
         <Text fontWeight="600" fontSize="36px" color="black" pb="3">
           Create profile
         </Text>
@@ -53,7 +70,11 @@ export default function Index() {
           handle
         </Text>
         <InputGroup size="md">
-          <Input placeholder="example" />
+          <Input
+            placeholder="example"
+            value={handle}
+            onChange={handleChangehandle}
+          />
           <InputRightAddon children=".test" />
         </InputGroup>
 
@@ -61,22 +82,20 @@ export default function Index() {
           profile picture
         </Text>
         <InputGroup size="md">
-          <Input placeholder="https://example.com" />
+          <Input placeholder="null" isDisabled />
         </InputGroup>
 
         <Text fontWeight="400" fontSize="20px" color="lensDark" pb="3" pt="10">
           follow NFT
         </Text>
-        <Select placeholder="Select followNFTURI">
-          <option value="null">null</option>
-        </Select>
+        <Select placeholder="null"></Select>
 
         <Text fontWeight="400" fontSize="20px" color="lensDark" pb="3" pt="10">
           follow Module
         </Text>
-        <Select placeholder="Select followModule">
-          <option value="null">null</option>
-          <option value="FlowmiFollowModule">FlowmiFollowModule</option>
+        <Select placeholder="null">
+          {/* <option value="null">null</option>
+          <option value="FlowmiFollowModule">FlowmiFollowModule</option> */}
         </Select>
 
         <Center>
@@ -85,8 +104,7 @@ export default function Index() {
             borderRadius="10px"
             boxShadow="0px 2px 3px rgba(0, 0, 0, 0.15)"
             mt="10"
-
-            //   onClick={onOpenFollow}
+            onClick={handleCreateProfile}
           >
             <Flex>
               <Box w="40px" h="40px">
