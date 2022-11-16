180f32085daf74f1aa7e1b82f6223a856fc3ab29
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-11-16 03:33:22 -0300

    feat: added responsive in AccumulatedProfiles

--- app/components/landing/AccumulatedProfiles.tsx
+++ app/components/landing/AccumulatedProfiles.tsx
@@ -2,12 +2,12 @@ import { Avatar, Box, Center, Flex, Image, Text } from "@chakra-ui/react";
 import { transformToIpfsUrl } from "~/web3/ipfs";
 
 const AccumulatedProfiles = () => {
-  const HotProfilesArr = [
+  const hotProfilesArr = [
     {
       handle: "pailita.test",
       name: "Pailita",
       image: null,
-      accumulatedTokens: 2,
+      accumulatedTokens: 73,
     },
     {
       handle: "magnanimus.test",
@@ -39,9 +39,9 @@ const AccumulatedProfiles = () => {
         Accumulated profiles
       </Text>
 
-      <Center>
+      <Center display={["none", "block", "block", "block"]}>
         <Flex justify="space-between">
-          {HotProfilesArr.map((item) => {
+          {hotProfilesArr.map((item) => {
             return (
               <Box
                 key={item.handle}
@@ -125,6 +125,79 @@ const AccumulatedProfiles = () => {
               </Box>
             );
           })}
+        </Flex>
+      </Center>
+
+      <Center>
+        <Flex justify="space-between">
+          <Box bg="white" borderRadius="10px" width="320px" m="5">
+            <Flex justifyContent="space-between" p="4">
+              <Box width="50%">
+                <Flex>
+                  <Avatar
+                    size="md"
+                    name={hotProfilesArr[1].handle}
+                    src={transformToIpfsUrl(hotProfilesArr[1].image)}
+                    my="auto"
+                  />
+
+                  <Box my="auto" pl="2">
+                    <Text
+                      fontWeight="600"
+                      fontSize="12px"
+                      lineHeight="100%"
+                      letterSpacing="-0.03em"
+                      color="black"
+                      pt="1"
+                    >
+                      {hotProfilesArr[1].name}
+                    </Text>
+
+                    <Text
+                      fontWeight="600"
+                      fontSize="12px"
+                      lineHeight="100%"
+                      letterSpacing="-0.03em"
+                      bgGradient="linear(to-r, #31108F, #7A3CE3, #E53C79, #E8622C, #F5C144)"
+                      bgClip="text"
+                      pt="1"
+                    >
+                      @{hotProfilesArr[1].handle}
+                    </Text>
+                  </Box>
+                </Flex>
+              </Box>
+
+              <Box bg="lens" borderRadius="10px" w="35px" h="35px" my="auto">
+                <Image
+                  src="../assets/LOGO__lens_ultra small icon.png"
+                  alt="lens"
+                  my="-5px"
+                />
+              </Box>
+            </Flex>
+
+            <Text
+              textAlign="center"
+              fontWeight="700"
+              fontSize="14px"
+              lineHeight="100%"
+              letterSpacing="-0.03em"
+              color="black"
+              pl="5"
+              pr="5"
+              pb="5"
+            >
+              {hotProfilesArr[1].name} has accumulated{" "}
+              <Text
+                as="span"
+                bgGradient="linear(to-r, #31108F, #7A3CE3, #E53C79, #E8622C, #F5C144)"
+                bgClip="text"
+              >
+                {hotProfilesArr[1].accumulatedTokens} WMATIC
+              </Text>
+            </Text>
+          </Box>
         </Flex>
       </Center>
 
